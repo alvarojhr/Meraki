a81061cde140b0f045e95a8288a94bd81b039a57
hernandavidc <hernandavidc@hotmail.com>
2017-10-21 20:39:11 -0500

    navBar a la izq

--- Meraki/assets/js/app.js
+++ Meraki/assets/js/app.js
@@ -1,6 +1,3 @@
 $(document).ready(function() {
-    $(".left a.open").on("click", function(e) {
-      // get the <body>. Does it currently have a class of "open-nav"? If it does, remove the class, which will collapse the left column.' If it does not have a class of "open-nav", add it, which will expand the left column.
-      $("body").toggleClass("open-nav");
-    });
+
 } );
